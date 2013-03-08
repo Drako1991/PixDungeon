5d6d87783c303a56f0e633f12e5e6d1e4a6f2d16
Zurrox <zurrox@live.com>
2013-03-08 19:13:13 +0000

    Added more search array code and a way to detect whether a string or int is in the array.

--- src/BaseClasses/Functions.java
+++ src/BaseClasses/Functions.java
@@ -1,14 +1,39 @@
 package BaseClasses;
 
 public class Functions {
-    public int findIndexWithString(String[] s, String in) {
+    public static int findStringIndex(String[] s, String in) {
         for(int i = 0; i <= s.length; i++) {
             if(s[i] == in) {
                 return i;
-            }else{
-                return -1;
             }
         }
         return -1;
+    }
+
+    public static int findIntIndex(int[] s, int in) {
+        for(int i = 0; i <= s.length; i++) {
+            if(s[i] == in) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static boolean doesStringExist(String[] s, String in) {
+        for(int i = 0; i <= s.length; i++) {
+            if(s[i] == in) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean doesIntExist(int[] s, int in) {
+        for(int i = 0; i <= s.length; i++) {
+            if(s[i] == in) {
+                return true;
+            }
+        }
+        return false;
     }
 }
