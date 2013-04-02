51a67b6105a0fee656838934382a741a0e8c1308
Zurrox <zurrox@live.com>
2013-04-02 02:30:52 +0100

    Adjusted the directory so it matches the changes in the location of music.

--- src/Audio/MusicList.java
+++ src/Audio/MusicList.java
@@ -1,5 +1,5 @@
 package Audio;
 
 public class MusicList {
-    public static GameMusic cave1 = new GameMusic("res/cave1_wip1.wav");
+    public static GameMusic cave1 = new GameMusic("res/Music/cave/cave1_wip1.wav");
 }
