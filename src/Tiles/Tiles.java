84e72eeae089a5fb5de9195ac984a683c0173fe6
Zurrox <zurrox@live.com>
2013-04-05 19:48:34 +0100

    Path is no longer a solid tile. Improved formating.

--- src/Tiles/Tiles.java
+++ src/Tiles/Tiles.java
@@ -3,8 +3,9 @@ package Tiles;
 import Maps.TileList;
 
 public class Tiles extends BaseObject {
-    public static BaseObject grass = new BGObject(0, 0, "Grass", TileList.grass, false),
-            path = new BGObject(1, 0, "Path", TileList.path, true),
+    public static BaseObject
+            grass = new BGObject(0, 0, "Grass", TileList.grass, false),
+            path = new BGObject(1, 0, "Path", TileList.path, false),
             bGrass = new BGObject(0, 1, "bGrass", TileList.bGrass, false),
             tGrass = new BGObject(0, 2, "tGrass", TileList.tGrass, false),
             lGrass = new BGObject(0, 3, "lGrass", TileList.lGrass, false),
