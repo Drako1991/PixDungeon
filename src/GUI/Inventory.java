9a171afa5388633d39b2bc0e145fda4cbea05b94
Zurrox <zurrox@live.com>
2013-04-05 21:20:01 +0100

    Added initInventory to improve performance.

--- src/GUI/Inventory.java
+++ src/GUI/Inventory.java
@@ -1,6 +1,5 @@
 package GUI;
 
-import Objects.Item;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -9,24 +8,34 @@ import org.newdawn.slick.geom.Vector2f;
 
 public class Inventory {
     private static Slot[] slots = new Slot[42];
-    private static Item logoTest = new Item("logoTest", SlotTypes.Inventory, "res/logo.png");
+    private static Image bgImage;
+//    private static Item logoTest = new Item("logoTest", SlotTypes.Inventory, "res/logo.png");
 
-    public static void Inventory(GameContainer gc, Graphics g) {
-        for(int x = 0; x <= 5; x++) {
-            for(int y = 0; y <= 6; y++) {
-                slots[x + (y*5) + y] = new Slot(SlotTypes.Inventory, new Vector2f(gc.getWidth() - 75 -(x*50), gc.getHeight()/2 + y*50));
-                slots[x + (y*5) + y].setItemInSlot(logoTest);
-            }
-        }
-        try{
-            Image img = new Image("res/GUI/Inventory.png");
-            img.draw(gc.getWidth() - 332.5f, 21);
-        }catch(SlickException e) {
+    public static void initInventory(GameContainer gc) {
+        try {
+            bgImage = new Image("res/GUI/Inventory.png");
+        } catch(SlickException e) {
             e.printStackTrace();
         }
+        for(int x = 0; x<=5; x++) {
+            for(int y = 0; y<=6; y++) {
+                slots[x + (y * 5) + y] = new Slot(SlotTypes.Inventory, new Vector2f(gc.getWidth() - 75 - (x * 50), gc.getHeight() / 2 + y * 50));
+            }
+        }
+    }
 
+    public static void Inventory(GameContainer gc, Graphics g) {
+        bgImage.draw(gc.getWidth() - 332.5f, 21);
         for(int i = 0; i < slots.length; i++) {
             slots[i].update(gc, g);
         }
+    }
+
+    public static int getMaxSlots() {
+        return slots.length;
+    }
+
+    public static Slot getSlot(int ID) {
+        return slots[ID];
     }
 }
