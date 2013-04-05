b6b322a39aedbb16fdbe7899afd9814c9ff098b2
Zurrox <zurrox@live.com>
2013-04-05 14:13:35 +0100

    Slot's can be rendered and updated & also the scale of them can be changed.

--- src/GUI/Slot.java
+++ src/GUI/Slot.java
@@ -1,5 +1,10 @@
 package GUI;
 
+import Objects.Item;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Vector2f;
 
 public class Slot {
@@ -7,6 +12,7 @@ public class Slot {
     private float scale = 64;
     private boolean isHoveredOver;
     private SlotTypes type;
+    private Item itemInSlot;
 
     public Slot(SlotTypes Type, Vector2f Pos) {
         pos = Pos;
@@ -19,6 +25,16 @@ public class Slot {
         type = Type;
     }
 
+    public Slot setScale(float Scale) {
+        scale = Scale;
+        return this;
+    }
+
+    public Slot setItemInSlot(Item item) {
+        itemInSlot = item;
+        return this;
+    }
+
     public SlotTypes getType() {
         return type;
     }
@@ -26,6 +42,38 @@ public class Slot {
     public Slot setHovered(boolean hover) {
         isHoveredOver = hover;
         return this;
+    }
+
+    public Slot render(Graphics g) {
+        if(isHoveredOver) {
+            g.setColor(new Color(255, 255, 255, 100));
+            g.fill(new Rectangle(pos.x, pos.y, scale, scale));
+        }
+        if(itemInSlot != null) {
+            itemInSlot.getImage().draw(pos.x, pos.y, scale, scale);
+        }
+        return this;
+    }
+
+    public Slot update(GameContainer gc) {
+        if(gc.getInput().getMouseX() >= pos.x && gc.getInput().getMouseX() <= pos.x + scale) {
+            if(gc.getInput().getMouseY() >= pos.y && gc.getInput().getMouseY() <= pos.y + scale) {
+                isHoveredOver = true;
+            }else isHoveredOver = false;
+        }else isHoveredOver = false;
+        return this;
+    }
+
+    public Vector2f getPos() {
+        return pos;
+    }
+
+    public float getScale() {
+        return scale;
+    }
+
+    public Item getItemInSlot() {
+        return itemInSlot;
     }
 
     public boolean getHovered() {
