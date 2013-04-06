22d1a0c294ef131d677a1c9583f3dfa87b51106f
Zurrox <zurrox@live.com>
2013-04-06 01:40:48 +0100

    Basic Mob code & Beginnings to AI.

---
+++ src/AI/TrollAI.java
@@ -0,0 +1,11 @@
+package AI;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Charlie
+ * Date: 06/04/13
+ * Time: 01:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class TrollAI {
+}
---
+++ src/Mobs/Mob.java
@@ -0,0 +1,11 @@
+package Mobs;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Charlie
+ * Date: 06/04/13
+ * Time: 00:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class Mob {
+}
